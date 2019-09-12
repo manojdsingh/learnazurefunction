a26f261d8a1b542a908a815e0f957d15f8c355b7
manoj <manojdsingh2000@yahoo.co.in>
2019-09-12 22:53:50 +0530

    add to git

--- HttpTrigger.cs
+++ HttpTrigger.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
 
 namespace My.Functions
 {
@@ -15,6 +16,7 @@ namespace My.Functions
         [FunctionName("HttpTrigger")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [Queue("outqueue"),StorageAccount("AzureWebJobsStorage")] ICollector<string> msg,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.1");
@@ -24,6 +26,11 @@ namespace My.Functions
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            if (string.IsNullOrEmpty(name) == false)
+            {
+                msg.Add(string.Format("Name passed to the function: {0}", name));
+            }
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
